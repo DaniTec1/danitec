f050d06a76eb2b7115da648b0c913b91143ad7ad
Dani-Rosas753 <64808943+Dani-Rosas753@users.noreply.github.com>
2021-04-17 19:15:48 -0500

    Compile mi sitio web

---
+++ docs/precache-manifest.015492ab5d966dd7969293c7c1a44691.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3b608657fcc53c605b",
+    "url": "/css/app.9d524448.css"
+  },
+  {
+    "revision": "2ec9e87f4add7f22f9537354ddab1e78",
+    "url": "/img/Banner1.2ec9e87f.png"
+  },
+  {
+    "revision": "883deb785a4b1e0063985e233e749a7a",
+    "url": "/img/Banner2.883deb78.png"
+  },
+  {
+    "revision": "2762990977d090118844797487333deb",
+    "url": "/img/Banner3.27629909.png"
+  },
+  {
+    "revision": "61664873b4dd02f95e2400f3abcf7ce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3b608657fcc53c605b",
+    "url": "/js/app.94a9a0f7.js"
+  },
+  {
+    "revision": "7d294b4df7196f51592d",
+    "url": "/js/chunk-vendors.7b990b82.js"
+  },
+  {
+    "revision": "f0a26edaac70284ee23c92133810c45f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
